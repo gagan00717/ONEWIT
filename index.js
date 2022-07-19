803f738ff8463ecbfa5134f388f6a526943eccaa
M GAGAN <95357401+gagan00717@users.noreply.github.com>
2022-07-19 19:46:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const menuItem = document.querySelectorAll('.menu-item');
 
+const menuItems = document.querySelectorAll('.menu-item');
+const messagesNotifiaction = document.querySelector('#message-notification');
+const messageS = document.querySelector('.messageS');
+const message = document.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
 
 const changeActiveItem = () => {
-    menuItem.forEach(item => {
+    menuItems.forEach(item => {
         item.classList.remove('active');
     })
 }
-menuItem.forEach(item => {
-    item, addEventListener('click', () => {
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
         changeActiveItem();
         item.classList.add('active'); 
         if (item.id != 'notifications') {
@@ -17,9 +25,68 @@ menuItem.forEach(item => {
         else {
             document.querySelector('.notifications-popup').
                 style.display = 'block';
-            document.querySelector('#notifications.')
+            document.querySelector('#notifications .notification-count').style.display = 'none';
+           
             
         }
        
     })
 })
+const searchMessage = () => {
+    const val = messageSearch.ariaValueMax.toLowerCase();
+    console.log(val);
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if (name.indexOf(val) != -1) {
+            chat.style.display = 'flex';
+        }
+        else {
+            chat.style.display = 'none';
+        }
+    })
+}
+
+
+
+messageSearch.addEventListener('keyup',searchMessage)
+
+messagesNotifiaction.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotifiaction.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+
+    }, 2000);
+})
+
+
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+
+const closeThemeModal = (e) => {
+    if (e.target.classList.contains('customize-theme')) {
+        themeModal.style.display = 'none';
+    }
+
+}
+
+
+themeModal.addEventListener('click', closeThemeModal);
+theme.addEventListener('click', openThemeModal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
