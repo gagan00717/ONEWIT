89530fc3f8cdaceecf20d3ccb6010e8988e9294c
Manoj546 <manoj.cs20@bmsce.ac.in>
2022-07-26 23:40:58 +0530

    collapsible

--- collapsible.js
+++ collapsible.js
@@ -1,11 +1,12 @@
-let coll = document.getElementsByClassName("collapsible__button")[0];
-
-coll.addEventListener("click", function () {
-  this.classList.toggle("content--active");
-  var content = this.nextElementSibling;
-  if (content.style.maxHeight) {
-    content.style.maxHeight = null;
-  } else {
-    content.style.maxHeight = "700px";
-  }
-});
+const coll = document.getElementsByClassName("collapsible__button");
+for(i=0;i<coll.length;i++){
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("content--active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = "700px";
+    }
+  });
+}
