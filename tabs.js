5c1005af84c8f6f39ae67a0299432f2a30464e06
Manoj <manoj.cs20@bmsce.ac.in>
2022-07-28 20:52:31 +0530

    Profile complete
    28/07/22

--- tabs.js
+++ tabs.js
@@ -1,10 +1,12 @@
+var tabs = document.getElementsByClassName("tabcontent");
+tabs[0].style.display = "block";
 function openCity(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
+  for (i = 1; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
@@ -13,7 +15,7 @@ function openCity(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-
+  tabcontent[0].style.display = "none";
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
